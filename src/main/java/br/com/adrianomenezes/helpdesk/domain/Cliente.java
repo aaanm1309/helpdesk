e8a34160a7e87003adcb6938305266eba8a06785
Adriano Menezes <aaanm1309@gmail.com>
2023-01-31 16:14:58 -0300

    incluindo anotacoes

--- src/main/java/br/com/adrianomenezes/helpdesk/domain/Cliente.java
+++ src/main/java/br/com/adrianomenezes/helpdesk/domain/Cliente.java
@@ -3,20 +3,28 @@ package br.com.adrianomenezes.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.adrianomenezes.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter@Setter
 public class Cliente extends Pessoa {
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     
--- src/main/java/br/com/adrianomenezes/helpdesk/domain/Tecnico.java
+++ src/main/java/br/com/adrianomenezes/helpdesk/domain/Tecnico.java
@@ -1,4 +1,35 @@
 package br.com.adrianomenezes.helpdesk.domain;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.adrianomenezes.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter@Setter
 public class Tecnico extends Pessoa{
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "tecnico")
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico() {
+        super();
+        addPerfil(Perfil.TECNICO);
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+        super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.TECNICO);
+    }
+
+
+    
+
 }
